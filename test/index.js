39d904b31dcf92e7b847bc6c31ae94d82ba7ec39
Jeff <malnick@gmail.com>
2014-10-22 13:29:23 -0700

    testing yaya

--- test/index.js
+++ test/index.js
@@ -5,27 +5,26 @@ var myServer = require('../index');
 
 describe('An HTTP Server', function() {
 
-	beforeEach(function(done) {
+	before(function(done) {
 		freeport(function(err, port) {
 			this.uri = 'http://localhost:'+ port;
 			myServer.listen(port, done)
 		}.bind(this))
-	done()
-	})
+	});
 
-	it('should make a request', function(done) {
+	it('server should be responding 200', function(done) {
 		console.log("Testing server availability:")
 		console.log(this.uri)
 		request(this.uri, function(err, resp, body) {
 			if (err) {
 				throw err;
 			}
-		assert.equal(resp.statusCode, 200)
-		done()
+			assert.equal(resp.statusCode, 200)
+			done()
 		})
 	})
 
-	it('should return the value of arbitrary shell command', function() {
+	it('should return the value of arbitrary shell command', function(done) {
 		// Need real value of command to pass to REST interface in 
 		request.put({
 			url: this.uri,
@@ -38,6 +37,7 @@ describe('An HTTP Server', function() {
 			assert.equal(resp.statusCode, 200)
 			// Ensure the body is stringy
 			assert.typeOf(body, string)
+			done()
 		}) 
-	})
+	});
 });
